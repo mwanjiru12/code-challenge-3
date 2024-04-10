13e7e2b932e802be3393bfce25d2bb65e69ca013
Maria Kamau <maria.kamau@student.moringaschool.com>
2024-04-11 00:10:46 +0300

    Replace hardcoded first movie URL with dynamic one from films API; add filmsMenu function to display film titles in a list

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-const firstMovieUrl = 'http://localhost:3000/films/1';
+const filmsFullApi = 'http://localhost:3000/films';
+const firstMovieUrl = `${filmsFullApi}/1`;
+const filmsList = el('films')
 
 fetch(firstMovieUrl)
 .then (res => res.json())
 .then(showMovieInfo);
+
+fetch(filmsFullApi)
+.then (res => res.json())
+.then(filmsMenu);
+
+function filmsMenu(films){
+  filmsList.innerHTML = '';
+  films.forEach(film => {
+    const filmLi = document.createElement('li')
+    filmLi.textContent = film.title;
+    filmLi.classList.add('film');
+    filmLi.classList.add('item');
+    filmsList.appendChild(filmLi);
+  });
+}
 
 function showMovieInfo(movie) {
   el('title').textContent = movie.title;
